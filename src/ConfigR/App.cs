7b4d0b984ca178694a2f7e5e6bd7ef14ca5e3646
Rajesh Guptha <rajeshgupthar@gmail.com>
2013-10-13 16:33:22 +0530

    Implemented first version of changes required for dynamic properties in csx files. Due to be pushed only after the new release of Roslyn CTP. Current version doesnt support dynamic objects - http://shar.es/EGF3Y

---
+++ src/ConfigR/App.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+namespace ConfigR
+{
+    public class App : DynamicObject
+    {
+        private static dynamic _appInstance = new App();
+        public static dynamic Settings
+        {
+            get
+            {
+                return _appInstance;
+            }
+        }
+
+        protected App()
+        {
+        }
+
+        private Dictionary<string, dynamic> _settings = new Dictionary<string, dynamic>();
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            if (!_settings.ContainsKey(binder.Name))
+            {
+                _settings.Add(binder.Name, value);
+                return true;
+            }
+            return base.TrySetMember(binder, value);
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            if (_settings.ContainsKey(binder.Name))
+            {
+                result = _settings[binder.Name];
+                return true;
+            }
+            return base.TryInvokeMember(binder, args, out result);
+        }
+    }
+}
--- src/sample/ConfigR.ConsoleApplication/Custom1.csx
+++ src/sample/ConfigR.ConsoleApplication/Custom1.csx
@@ -2,3 +2,6 @@
     .Add("Count", 234)
     .Add("Uri", new Uri("https://github.com/config-r/config-r/foobarbaz"))
     .Add("FromCustom1File", true);
+
+App.Settings.MySetting = "23";
+App.Settings.NewSetting = 12;
